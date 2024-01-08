ee6349b4426ec5139e0be7deab15640e672097fa
Akhand Pratap Singh <pratapsinghakhand24@gmail.com>
2024-01-09 01:18:41 +0530

    updated code in healthcheck

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -5,9 +5,19 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const healthcheck = asyncHandler(async (req, res) => {
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
-    return res.status(200).json({
-        message: "Server is healthy."
-    })
+    try {
+        return res.status(200).json(
+            new ApiResponse(
+                200, 
+                {
+                    info : "Server is up and running"
+                },
+                "User logged In Successfully"
+            )
+        )
+    } catch (error) {
+        return res.status(500).json(new ApiError(500, "Something wrong in server"))
+    }
 })
 
 export {
